67981a66900994321529364904d2f7e8452623dd
maxiaofeng <markma@rams.colostate.edu>
2020-01-28 19:37:45 -0700

    fix syntax in mice_time_series_example.py and LoadMiceTimeSeriesData.py

--- LoadMiceTimeSeriesData.py
+++ LoadMiceTimeSeriesData.py
@@ -1,10 +1,10 @@
-'''
+"""
 LoadMiceTimeSeriesData is used to load and clean tamu time series data. The purpose is to use the processed data to
 train BiDirectionalLSTM model for classification(as of now). (TODO: GIVE AN EXAMPLE)
-'''
+"""
 import calcom.io.CCDataSet as ccLoad
 import numpy as np
-import pdb
+
 
 class MiceLoader:
     def __init__(self):
@@ -24,8 +24,8 @@ class MiceLoader:
     def load_ccd(self, path):
         self.ccd = ccLoad(path)
 
-    def get_ccDataInDict(self,CCLines, time_interval, embedding):
-        '''
+    def get_cc_data_in_dict(self, CCLines, time_interval, embedding):
+        """
         :param CCLines: A list of CCLine names
         :param time_interval: time interval for each sample
         :param embedding: [dim, delay] dim: integer embedding dimention delay: delay time
@@ -35,83 +35,85 @@ class MiceLoader:
                  Label: (#samples,2) 2-d list: pre-inoculation[1,0] or after-inoculation[0,1]
                  Interval: (#samples, time_interval) 2-d list: [start:end] start to end of the time series relative to
                             inoculation time
-        '''
+        """
         self.CCLines = CCLines
         self.time_interval = time_interval
         self.embed_dim, self.delay = embedding
         # get all idx corresponding to given CCLines
-        lineIdx = self.ccd.find_attr_by_value('line',self.CCLines)
+        line_idx = self.ccd.find_attr_by_value('line', self.CCLines)
         # generate dictionary
         self.data_dict = {name: self.get_mice_data(idx)
-                          for name,idx in zip(self.ccd.get_attr_values('mouse_id',idx=lineIdx),lineIdx)}
+                          for name, idx in zip(self.ccd.get_attr_values('mouse_id', idx=line_idx), line_idx)}
         return self.data_dict
 
-    def get_ccDataInDict_full(self,CCLines):
-        '''
+    def get_cc_data_in_dict_full(self, CCLines):
+        """
         This function takes in CClines and return dictionary contains the full time series data
         :param CCLines: A list of CC lines
-        :return: dict_ts: A dictionary: key values are mouse names, each item is a 1-d array(full time series corresponding to that mouse)
-        '''
+        :return: dict_ts: A dictionary: key values are mouse names, each item is a 1-d array(full time series
+                 corresponding to that mouse)
+        """
         self.CCLines = CCLines
-        lineIdx = self.ccd.find_attr_by_value('line', self.CCLines)
-        self.data_dict_ts = {name: self.get_full_time_series(idx) for name,idx in zip(self.ccd.get_attr_values('mouse_id',idx=lineIdx),lineIdx)}
+        line_idx = self.ccd.find_attr_by_value('line', self.CCLines)
+        self.data_dict_ts = {name: self.get_full_time_series(idx) for name, idx
+                             in zip(self.ccd.get_attr_values('mouse_id', idx=line_idx), line_idx)}
         return self.data_dict_ts
 
-    def get_mice_data(self,idx):
-        '''
+    def get_mice_data(self, idx):
+        """
         This function takes in an index in self.ccd and output list: [Data,Label,Interval]
                  Data: (#samples,#time-steps) 2-d array
                  Label: (#samples,2) 2-d list: pre-inoculation[1,0] or after-inoculation[0,1]
                  Interval: (#samples, time_interval) 2-d list: [start:end] start to end of the time series relative to
                             inoculation time
-        :param idx: index of data points in ccd dataset
+        :param idx: index of data points in ccd data set
         :return: List: [Data, label, Interval]
-        '''
+        """
         # infection time or inoculation time, can think of it as 'origin' of time series
-        infection_time = self.ccd.get_attr_values('infection_time',idx=[idx])
+        infection_time = self.ccd.get_attr_values('infection_time', idx=[idx])
         # time steps to capture time series
         T = self.time_interval
         # get ccd data matrix
         data_matrix = self.ccd.generate_data_matrix(idx=[idx])
         # clean temperature data
-        non_nan_data, time_idx = self.clean_data(data_matrix[0,0,:])
+        non_nan_data, time_idx = self.clean_data(data_matrix[0, 0, :])
         # parse data
-        parsed_data, labels, intervals = self.parse_data(non_nan_data,time_idx,infection_time)
+        parsed_data, labels, intervals = self.parse_data(non_nan_data, time_idx, infection_time)
         return [parsed_data, labels, intervals]
 
-    def get_full_time_series(self,idx):
+    def get_full_time_series(self, idx):
         """
         This function takes in an idx in self.ccd and output full time series
         :param idx: index of data poitns in ccd dataset
         :return: 1-d array: full time series
         """
-        data_mat = self.ccd.generate_data_matrix(idx = [idx])
-        non_nan_data, time_idx = self.clean_data(data_mat[0,0,:])
+        data_mat = self.ccd.generate_data_matrix(idx=[idx])
+        non_nan_data, time_idx = self.clean_data(data_mat[0, 0, :])
         # smooth data
         return non_nan_data
 
     def clean_data(self, time_series):
-        '''
+        """
         Clean nan value and keep time steps
         :param time_series: 1d array time series data
         :return:
-        '''
+        """
         non_nan_idx = ~np.isnan(time_series)
-        non_nan_array, idx = time_series[non_nan_idx] ,np.arange(len(time_series))[non_nan_idx]
-        non_nan_array =self.smooth_time_series(non_nan_array,self.num_coeffs)
+        non_nan_array, idx = time_series[non_nan_idx], np.arange(len(time_series))[non_nan_idx]
+        non_nan_array = self.smooth_time_series(non_nan_array, self.num_coeffs)
         return non_nan_array, idx
 
     def parse_data(self, data_mat, raw_idx, infection_time):
-        '''
+        """
         parse data_mat as a list of time segments attach pre-inoculation and post-inoculation label to data
         :param data_mat: 1d array of time series
         :param raw_idx: original time idx
         :param infection_time: inoculation time
         :return: a list of time segments, labels and corresponding time stamps
-        '''
+        """
         # calculate infection index
         data_mat, raw_idx = self.time_delayed_embedding(data_mat,raw_idx)
-        infection_idx = np.where(raw_idx>=infection_time)[0][0]
+        infection_idx = np.where(raw_idx >= infection_time)[0][0]
         T = self.time_interval
         # prepare pre and post infection data
         pre_len = infection_idx-self.delay*self.embed_dim-T
@@ -121,15 +123,16 @@ class MiceLoader:
                                for i in range(post_len)]
         # return data and corresponding label [1,0]: pre-inoculation [0,1]:post-inoculation
         return pre_infection_data+post_infection_data, \
-               [[1,0]]*len(pre_infection_data)+[[0,1]]*len(post_infection_data),\
-                [raw_idx[np.arange(i, i+T)] for i in range(pre_len)] + \
-                [raw_idx[np.arange(infection_idx+i, infection_idx + i + T)] for i in range(post_len)]
+            [[1, 0]]*len(pre_infection_data)+[[0, 1]]*len(post_infection_data),\
+            [raw_idx[np.arange(i, i+T)] for i in range(pre_len)] + \
+            [raw_idx[np.arange(infection_idx+i, infection_idx + i + T)] for i in range(post_len)]
 
     def time_delayed_embedding(self, x, raw_idx):
-        '''
+        """
         :param x: time series data, (n,) vector
+        :param raw_idx: time index for original data, (n,) vector
         :return: (x_delayed, new_idx)
-        '''
+        """
         dim = self.embed_dim
         delay = self.delay
         embed_data = []
@@ -141,13 +144,13 @@ class MiceLoader:
             new_idx.append(raw_idx[idx[-1]])
         return np.array(embed_data), np.array(new_idx)
 
-    def smooth_time_series(self,x,num_coefs):
-        '''
+    def smooth_time_series(self, x, num_coefs):
+        """
         This function takes in a time series data, output a smooth version by pass filter
         :param x: 1-d array
         :param num_coefs: integer first num_coefs are kept and the rest is set to be zero
         :return: smooth_x: 1-d array , smoothed version of x
-        '''
+        """
         rbf = np.fft.rfft(x)
         rbf[num_coefs:] = 0
         smooth_x = np.fft.irfft(rbf)
--- mice_time_series_example.py
+++ mice_time_series_example.py
@@ -7,66 +7,57 @@ import numpy as np
 import pickle
 import LoadMiceTimeSeriesData as loadMice
 
-
-
 # ----------------------------Loading data--------------------------------------
-path ='/data3/darpa/all_CCD_processed_data/tamu_expts_01-28.h5'
+path = '/data3/darpa/all_CCD_processed_data/tamu_expts_01-28.h5'
 loader = loadMice.MiceLoader()
 loader.load_ccd(path)
 
 lineName = ['CC001']
 segment_interval = 120
-embedding = [2,60]
-miceDict = loader.get_ccDataInDict('CC001',segment_interval,embedding)
+embedding = [2, 60]
+miceDict = loader.get_cc_data_in_dict('CC001', segment_interval, embedding)
 # ----------------------------Prepare mice Data --------------------------------
+
+# TODO: rewrite create_inout_seq function
 
 
 def create_inout_seq(miceDict):
     input_seq = []
     for i in miceDict.keys():
         for j in range(len(miceDict[i][0])):
-            train_in, train_out = torch.from_numpy(np.array(miceDict[i][0][j])).float(), torch.from_numpy(np.array(miceDict[i][1][j])).float()
+            train_in, train_out = torch.from_numpy(np.array(miceDict[i][0][j])).float(), \
+                                  torch.from_numpy(np.array(miceDict[i][1][j])).float()
             input_seq.append((train_in, train_out))
     return input_seq
 
 
 inout_seq = create_inout_seq(miceDict)
-pdb.set_trace()
-#switch_idx = []
-#for i in range(len(inout_seq)-1):
-#    if torch.norm(inout_seq[i][1]-inout_seq[i+1][1]) > 0.001:
-#        switch_idx.append(i + 1)
-#        continue
-#print(switch_idx)
-#pdb.set_trace()
+
 batch_size = 64
 batch_num = 248
 train_num = batch_size * batch_num
-test_num =1
+test_num = 1
 
 
-def create_batch_data(inout_seq,test_num,batch_num,batch_size):
-    #train_num = batch_num*batch_size
-    #rand_idx = np.random.permutation(train_num)
-    #inout_seq_permute = [inout_seq[i] for i in rand_idx]
+def create_batch_data(inout_seq, batch_number, batch_volume):
     inout_seq_permute = inout_seq
     batch_data = []
     batch_label = []
-    for i in np.arange(batch_num):
-        pair_i = inout_seq_permute[i*batch_size:(i+1)*batch_size]
-        batch_i,label_i = [k[0].tolist() for k in pair_i],[ll[1].tolist() for ll in pair_i]
+    for i in np.arange(batch_number):
+        pair_i = inout_seq_permute[i*batch_size:(i+1)*batch_volume]
+        batch_i, label_i = [kk[0].tolist() for kk in pair_i], [ll[1].tolist() for ll in pair_i]
         # TODO: resize batch data for LSTM input
         batch_data.append(batch_i)
         batch_label.append(label_i)
-    return torch.tensor(batch_data).float(),torch.tensor(batch_label).float()
+    return torch.tensor(batch_data).float(), torch.tensor(batch_label).float()
 
 
-batch_train,batch_train_label = create_batch_data(inout_seq, test_num,batch_num, batch_size)
+batch_train, batch_train_label = create_batch_data(inout_seq, batch_num, batch_size)
 
 
 # ----------------------------Run LSTM Model -----------------------------------
 hidden_layer_size = 128
-lstm_model = biLstm.BiLSTM(batch_size,input_size=embedding[0],hidden_size=hidden_layer_size,output_size=2)
+lstm_model = biLstm.BiLSTM(batch_size, input_size=embedding[0], hidden_size=hidden_layer_size, output_size=2)
 lstm_model = lstm_model.float()
 loss_function = nn.BCELoss()
 print(lstm_model.parameters)
@@ -76,22 +67,19 @@ epoch = 1000
 for i in range(epoch):
     batch_err = 0
     for j in np.arange(batch_num):
-        seq, label = batch_train[j],batch_train_label[j]
+        seq, label = batch_train[j], batch_train_label[j]
         optimizer.zero_grad()
-        lstm_model.hidden_cell=(torch.zeros(2, batch_size, hidden_layer_size),
-                                torch.zeros(2, batch_size, hidden_layer_size))
+        lstm_model.hidden_cell = (torch.zeros(2, batch_size, hidden_layer_size),
+                                  torch.zeros(2, batch_size, hidden_layer_size))
         lstm_model.hidden_cell2 = (torch.zeros(1, batch_size, hidden_layer_size),
                                    torch.zeros(1, batch_size, hidden_layer_size))
         ypred = lstm_model(seq)
-        single_loss = loss_function(ypred,label)
+        single_loss = loss_function(ypred, label)
         single_loss.backward()
         optimizer.step()
-        print('%5d th batch ---- error %8.6f'%(j,single_loss.item()))
+        print('%5d th batch ---- error %8.6f' % (j, single_loss.item()))
         batch_err = batch_err + single_loss.item()
-    print(ypred)
-    print(label)
-
-    print('epoch:%5d, batch_error: %8.5f'%(i,batch_err))
+    print('epoch:%5d, batch_error: %8.5f' % (i, batch_err))
 
 pred_label = []
 true_label = []
@@ -100,16 +88,16 @@ for k in range(train_num, test_num+train_num):
     lstm_model.hidden_cell = (torch.zeros(2, 1, hidden_layer_size),
                               torch.zeros(2, 1, hidden_layer_size))
     lstm_model.batch_size = 1
-    pred_label.append(lstm_model(test_seq.reshape(1, segment_interval,1)).detach().numpy())
+    pred_label.append(lstm_model(test_seq.reshape(1, segment_interval, 1)).detach().numpy())
     true_label.append(truth_label.detach().numpy())
 
-with open('pred_label.txt','wb') as fp:
-    pickle.dump(pred_label,fp)
+with open('pred_label.txt', 'wb') as fp:
+    pickle.dump(pred_label, fp)
 
 with open('true_label.txt', 'wb') as fp:
-    pickle.dump(true_label,fp)
+    pickle.dump(true_label, fp)
 
-with open('pred_label.txt','rb') as fp:
+with open('pred_label.txt', 'rb') as fp:
     predicted = pickle.load(fp)
 print(predicted)
 for pred in predicted:
@@ -121,13 +109,14 @@ true_negative = 0
 num_positive = 0
 num_negative = 0
 for i in range(test_num):
-    pred_label = [1,0] if predicted[i][0][0]>predicted[i][0][1] else [0,1]
+    pred_label = [1, 0] if predicted[i][0][0] > predicted[i][0][1] else [0, 1]
     if abs(true_label[i][0].item()-1) <= 0.001:
         num_negative += 1
         if pred_label[0] == 1:
             true_negative += 1
     else:
         num_positive += 1
-        if pred_label[1]==1:
-            true_positive+=1
-print('True Negative:%.3f,# of negative %d; True Positve:%.3f,# of negative %d'%(true_negative/num_negative,num_negative,true_positive/num_positive,num_positive))
+        if pred_label[1] == 1:
+            true_positive += 1
+print('True Negative:%.3f,# of negative %d; True Positive:%.3f,# of negative %d' %
+      (true_negative/num_negative, num_negative, true_positive/num_positive, num_positive))
